ab7d34787598547b4cebd4d1c878e29c76f54af5
Gabriel Wyss <gw1@gmx.ch>
2017-12-30 12:02:37 +0100

    better comments

--- src/main/java/interfaces/HashValue.java
+++ src/main/java/interfaces/HashValue.java
@@ -1,5 +1,22 @@
 /**
- * This is a interface class we use for the HashValue annotations (decide which fields of an objects for generating it's hash value).
+ *   Copyright 2018 Pascal Ammon, Gabriel Wyss
+ * 
+ * 	 Implementation eines anonymen Mobility Pricing Systems auf Basis eines Gruppensignaturschemas
+ * 
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * This is a interface class which is used for the HashValue annotations (decide which fields of an object is used for generating it's hash value).
  */
 
 package interfaces;
